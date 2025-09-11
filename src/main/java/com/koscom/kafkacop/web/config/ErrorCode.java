b94377ddb54ff9363c7b96721c0726d44df64e97
Kwon770 <sckwon770@gmail.com>
2025-09-11 14:23:50 +0900

    fix: Fix compile error

--- src/main/java/com/koscom/kafkacop/web/config/ErrorCode.java
+++ src/main/java/com/koscom/kafkacop/web/config/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     UNKNOWN_SERVER_ERROR("CM0001", "Unknown Server Error"),
     INVALID_INPUT_VALUE("CM0002", "Invalid Input Value"),
     METHOD_NOT_ALLOWED("CM0003", "Method Not Allowed"),
-    ACCESS_DENIED("CM0004", "Access Denied"),
+    ACCESS_DENIED("CM0004", "Access Denied");
 
     private final String code;
     private final String message;
